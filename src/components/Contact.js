e6036739fe97420d7df83de9a1816272deb29d4d
sriram <sriramjanjanamram@gmail.com>
2026-05-05 16:40:48 +0530

    new2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
 const Contact = () => {
-  const [isMobile, setIsMobile] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
     email: '',
